ccaa716b1d301ac3af583daa89f432673b09e992
Alexandre Leal <aleal4343@gmail.com>
2021-06-24 17:56:40 -0300

    feat: adding method ResolveQueueUrl + tests

--- aws/aws-sdk-go.v2/client/sqs/client.go
+++ aws/aws-sdk-go.v2/client/sqs/client.go
@@ -6,21 +6,29 @@ import (
 	"github.com/americanas-go/errors"
 	"github.com/americanas-go/log"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 // Client knows how to publish on sqs
 type Client interface {
 	Publish(ctx context.Context, input *sqs.SendMessageInput) error
+	ResolveQueueUrl(ctx context.Context, queueName string) (*string, error)
+}
+
+type sqsClient interface {
+	SendMessage(ctx context.Context, params *sqs.SendMessageInput, optFns ...func(*sqs.Options)) (*sqs.SendMessageOutput, error)
+	GetQueueUrl(ctx context.Context, params *sqs.GetQueueUrlInput, optFns ...func(*sqs.Options)) (*sqs.GetQueueUrlOutput, error)
 }
 
 // Client holds client and resource name
 type client struct {
-	client *sqs.Client
+	client    sqsClient
+	queueUrls map[string]*string
 }
 
 // NewClient returns a initialized client
 func NewClient(c *sqs.Client) Client {
-	return &client{c}
+	return &client{c, map[string]*string{}}
 }
 
 // Publish publish message on sns
@@ -42,4 +50,26 @@ func (c *client) Publish(ctx context.Context, input *sqs.SendMessageInput) error
 		Debug("message sent to sqs")
 
 	return nil
+}
+
+func (c *client) ResolveQueueUrl(ctx context.Context, queueName string) (*string, error) {
+	if queueUrl, ok := c.queueUrls[queueName]; ok {
+		return queueUrl, nil
+	}
+
+	result, err := c.client.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+		QueueName: aws.String(queueName),
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, errors.New("error resolving sqs queue url"))
+	}
+
+	if result == nil || result.QueueUrl == nil {
+		return nil, errors.New("error resolving sqs queue url")
+	}
+
+	c.queueUrls[queueName] = result.QueueUrl
+
+	return result.QueueUrl, nil
 }
