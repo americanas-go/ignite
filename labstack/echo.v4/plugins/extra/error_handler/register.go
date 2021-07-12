72bd0ccd149e2ec47c8180a9f2695ac92ccdb922
Mauricio Lopes <mauricio.lopes@b2wdigital.com>
2021-07-12 16:14:00 -0300

    fix: adds error converter middleware for echo
    Also refactors state code mapping in the error handler.
    
    closes #68

--- labstack/echo.v4/plugins/extra/error_handler/register.go
+++ labstack/echo.v4/plugins/extra/error_handler/register.go
@@ -8,6 +8,14 @@ import (
 	e "github.com/labstack/echo/v4"
 )
 
+// Register registers the error handler for echo.
+//
+// The error handler and converter middleware are registered.
+// The handler handles how the application's errors will be sent in the response.
+// And the converter converts application errors into echo errors.
+//
+// If the New Relic agent integration for Echo is used, this Register function must
+// be called after the New Relic agent Register function.
 func Register(ctx context.Context, server *echo.Server) error {
 	if !IsEnabled() {
 		return nil
@@ -17,6 +25,7 @@ func Register(ctx context.Context, server *echo.Server) error {
 	logger.Trace("configuring error handler in echo")
 
 	server.Instance().HTTPErrorHandler = errorHandler(server)
+	server.Instance().Use(errorConverter)
 
 	logger.Debug("error handler successfully configured in echo")
 
@@ -34,5 +43,13 @@ func errorHandler(server *echo.Server) func(err error, c e.Context) {
 			}
 		}
 
+	}
+}
+
+func errorConverter(next e.HandlerFunc) e.HandlerFunc {
+	return func(c e.Context) error {
+		err := next(c)
+		status := echo.ErrorStatusCode(err)
+		return e.NewHTTPError(status, err.Error())
 	}
 }
