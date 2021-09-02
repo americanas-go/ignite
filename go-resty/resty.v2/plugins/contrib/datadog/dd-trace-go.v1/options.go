c37ecc1778cfbec36afe3714e4b42c0d751403a2
João Vitor de Oliveira <joao.ocarlos@b2wdigital.com>
2021-09-02 18:48:11 -0300

    feat: parameterizes resty datadog plugin

---
+++ go-resty/resty.v2/plugins/contrib/datadog/dd-trace-go.v1/options.go
@@ -0,0 +1,39 @@
+package datadog
+
+import (
+	"github.com/americanas-go/config"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+)
+
+type Options struct {
+	Enabled       bool
+	OperationName string
+	SpanOptions   []ddtrace.StartSpanOption
+}
+
+func NewOptions(spanOptions ...ddtrace.StartSpanOption) (*Options, error) {
+	o := &Options{
+		SpanOptions: spanOptions,
+	}
+
+	err := config.UnmarshalWithPath(root, o)
+	if err != nil {
+		return nil, err
+	}
+
+	return o, nil
+}
+
+func NewOptionsWithPath(path string, spanOptions ...ddtrace.StartSpanOption) (opts *Options, err error) {
+	opts, err = NewOptions(spanOptions...)
+	if err != nil {
+		return nil, err
+	}
+
+	err = config.UnmarshalWithPath(path, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
