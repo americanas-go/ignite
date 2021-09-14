9c9453a53d55c5a75e2cae3a7f31da80b38f9fc7
João Paulo Faria <jpfaria@gmail.com>
2021-09-14 16:22:08 -0300

    chore: updates resty examples

--- go-resty/resty.v2/examples/multi/main.go
+++ go-resty/resty.v2/examples/multi/main.go
@@ -8,35 +8,36 @@ import (
 	ilog "github.com/americanas-go/ignite/americanas-go/log.v1"
 	"github.com/americanas-go/ignite/go-resty/resty.v2"
 	logplugin "github.com/americanas-go/ignite/go-resty/resty.v2/plugins/contrib/americanas-go/log.v1"
-	c "github.com/americanas-go/ignite/spf13/cobra.v1"
 	"github.com/americanas-go/log"
 	r "github.com/go-resty/resty/v2"
-	"github.com/spf13/cobra"
 )
 
 const (
 
 	// config google client
-	google = "app.resty.google"
+	googleConfigPath          = "app.resty.googleConfigPath"
+	googlePluginsConfigPath   = googleConfigPath + ".plugins"
+	googleLogPluginConfigPath = googlePluginsConfigPath + ".log"
 
 	// config americanas client
-	acom          = "app.resty.acom"
-	acomPlugins   = acom + ".plugins"
-	acomLogPlugin = acomPlugins + ".log"
+	acomConfigPath          = "app.resty.acomConfigPath"
+	acomPluginsConfigPath   = acomConfigPath + ".plugins"
+	acomLogPluginConfigPath = acomPluginsConfigPath + ".log"
 )
 
 func init() {
 
 	os.Setenv("IGNITE_LOGRUS_CONSOLE_LEVEL", "INFO")
 
-	os.Setenv("APP_RESTY_GOOGLE_HOST", "http://www.google.com")
+	os.Setenv("APP_RESTY_GOOGLE_HOST", "http://www.googleConfigPath.com")
 	os.Setenv("APP_RESTY_ACOM_HOST", "https://www.americanas.com.br")
 	os.Setenv("APP_RESTY_ACOM_PLUGINS_LOG_LEVEL", "INFO")
 
-	resty.ConfigAdd(google)
-	resty.ConfigAdd(acom)
+	resty.ConfigAdd(acomConfigPath)
+	logplugin.ConfigAdd(acomLogPluginConfigPath)
 
-	logplugin.ConfigAdd(acomLogPlugin)
+	resty.ConfigAdd(googleConfigPath)
+	logplugin.ConfigAdd(googleLogPluginConfigPath)
 }
 
 func main() {
@@ -47,61 +48,49 @@ func main() {
 	ctx := context.Background()
 	logger := log.FromContext(ctx)
 
-	cmds := []*cobra.Command{
-		{
-			Use:  "google",
-			Long: "google call",
-			RunE: func(cmd *cobra.Command, args []string) error {
-				return call(ctx, google)
-			},
-		},
-		{
-			Use:  "acom",
-			Long: "acom call",
-			RunE: func(cmd *cobra.Command, args []string) error {
-
-				acomLogP, err := logplugin.NewLogWithConfigPath(acomLogPlugin)
-				if err != nil {
-					return err
-				}
-
-				return call(ctx, acom, acomLogP.Register)
-			},
-		},
-	}
-
-	rootCMD := &cobra.Command{
-		Version: "1.0.0",
-	}
-
-	if err := c.Run(rootCMD, cmds...); err != nil {
-		logger.Errorf(err.Error())
-	}
-
-	// go run main.go -> show options
-	// go run main.go acom -> call acom
-	// go run main.go google -> call google
-}
-
-func call(ctx context.Context, path string, plugins ...resty.Plugin) error {
-
-	logger := log.FromContext(ctx)
-
 	var err error
 
-	var client *r.Client
-	if client, err = resty.NewClientWithConfigPath(ctx, path, plugins...); err != nil {
-		return err
+	// ACOM CALL
+
+	var acomLogPlugin *logplugin.Log
+	acomLogPlugin, err = logplugin.NewLogWithConfigPath(acomLogPluginConfigPath)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	var response *r.Response
-	if response, err = client.R().Get("/"); err != nil {
-		return err
+	var clientAcom *r.Client
+	if clientAcom, err = resty.NewClientWithConfigPath(ctx, acomConfigPath, acomLogPlugin.Register); err != nil {
+		log.Fatal(err)
 	}
 
-	if response != nil {
-		logger.Infof(response.String())
+	var responseAcom *r.Response
+	if responseAcom, err = clientAcom.R().Get("/"); err != nil {
+		log.Fatal(err)
 	}
 
-	return nil
+	if responseAcom != nil {
+		logger.Infof(responseAcom.String())
+	}
+
+	// GOOGLE CALL
+
+	var googleLogPlugin *logplugin.Log
+	if googleLogPlugin, err = logplugin.NewLogWithConfigPath(googleLogPluginConfigPath); err != nil {
+		log.Fatal(err)
+	}
+
+	var clientGoogle *r.Client
+	if clientGoogle, err = resty.NewClientWithConfigPath(ctx, googleConfigPath, googleLogPlugin.Register); err != nil {
+		log.Fatal(err)
+	}
+
+	var responseGoogle *r.Response
+	if responseGoogle, err = clientGoogle.R().Get("/"); err != nil {
+		log.Fatal(err)
+	}
+
+	if responseGoogle != nil {
+		logger.Infof(responseGoogle.String())
+	}
+
 }
