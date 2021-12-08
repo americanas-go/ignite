109f7dcc08a9ba53ed7194001d251e0dac8842a6
João Paulo Faria <jpfaria@gmail.com>
2021-12-08 19:42:37 -0300

    feat: adds incluster k8s client config

--- k8s.io/client-go.v0/config.go
+++ k8s.io/client-go.v0/config.go
@@ -3,8 +3,10 @@ package client
 import "github.com/americanas-go/config"
 
 const (
-	root           = "ignite.k8s"
-	kubeConfigPath = ".kubeConfigPath"
+	root              = "ignite.k8s"
+	tp                = ".type"
+	kubeConfigPath    = ".kubeConfigPath"
+	kubeConfigContext = ".kubeConfigContext"
 )
 
 func init() {
@@ -12,5 +14,7 @@ func init() {
 }
 
 func ConfigAdd(path string) {
+	config.Add(path+tp, "KUBECONFIG", "defines client type KUBECONFIG/INCLUSTER")
 	config.Add(path+kubeConfigPath, "~/.kube/config", "defines kubeconfig request")
+	config.Add(path+kubeConfigContext, "", "defines kubeconfig context")
 }
