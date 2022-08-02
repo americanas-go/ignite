3afe5621fd75213076e39d99b9a67407037f29d0
João Paulo Faria <jpfaria@gmail.com>
2022-08-02 20:27:30 -0300

    fix: fixes americanas-go/cache

--- coocood/freecache.v1/config.go
+++ coocood/freecache.v1/config.go
@@ -1,15 +1,12 @@
 package freecache
 
 import (
-	"time"
-
 	"github.com/americanas-go/config"
 )
 
 const (
 	root      = "ignite.freecache"
 	cacheSize = ".cacheSize"
-	ttl       = ".ttl"
 )
 
 func init() {
@@ -18,5 +15,4 @@ func init() {
 
 func ConfigAdd(path string) {
 	config.Add(path+cacheSize, 100*1024*1024, "The cache size will be set to 512KB at minimum")
-	config.Add(path+ttl, 5*time.Minute, "The cache size will be set to 512KB at minimum")
 }
