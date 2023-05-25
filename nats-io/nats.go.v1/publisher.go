ce7be11ab8ff35b5cb6373ee9f097ac0112b1e43
João Paulo Faria <jpfaria@gmail.com>
2023-05-24 23:26:44 -0300

    feat: removes internal plugin interface in favor of grapper

--- nats-io/nats.go.v1/publisher.go
+++ nats-io/nats.go.v1/publisher.go
@@ -6,59 +6,41 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
-// PublisherMiddleware defines an interface for a nats publisher plugin.
-type PublisherMiddleware interface {
-	Before(context.Context, *nats.Conn, *nats.Msg) (context.Context, error)
-	After(context.Context) error
-}
-
 // Publisher represents a nats publisher.
 type Publisher struct {
-	conn        *nats.Conn
-	options     *Options
-	middlewares []PublisherMiddleware
+	conn    *nats.Conn
+	options *Options
 }
 
 // NewPublisherWithConfigPath returns a new nats publisher with options from config path.
-func NewPublisherWithConfigPath(ctx context.Context, path string, middlewares ...PublisherMiddleware) (*Publisher, error) {
+func NewPublisherWithConfigPath(ctx context.Context, path string) (*Publisher, error) {
 	options, err := NewOptionsWithPath(path)
 	if err != nil {
 		return nil, err
 	}
-	return NewPublisherWithOptions(ctx, options, middlewares...)
+	return NewPublisherWithOptions(ctx, options)
 }
 
 // NewPublisherWithOptions returns a new nats publisher with options.
-func NewPublisherWithOptions(ctx context.Context, options *Options, middlewares ...PublisherMiddleware) (*Publisher, error) {
+func NewPublisherWithOptions(ctx context.Context, options *Options) (*Publisher, error) {
 	conn, err := NewConnWithOptions(ctx, options)
 	if err != nil {
 		return nil, err
 	}
-	return &Publisher{conn, options, middlewares}, nil
+	return &Publisher{conn, options}, nil
 }
 
 // NewPublisher returns a new nats publisher with default options.
-func NewPublisher(ctx context.Context, middlewares ...PublisherMiddleware) (*Publisher, error) {
+func NewPublisher(ctx context.Context) (*Publisher, error) {
 	options, err := NewOptions()
 	if err != nil {
 		return nil, err
 	}
-	return NewPublisherWithOptions(ctx, options, middlewares...)
+	return NewPublisherWithOptions(ctx, options)
 }
 
 // Publish publishes the message to nats.
 func (p *Publisher) Publish(ctx context.Context, msg *nats.Msg) error {
-
-	for _, middleware := range p.middlewares {
-		ctxx, err := middleware.Before(ctx, p.conn, msg)
-		if err != nil {
-			return err
-		}
-		defer func() {
-			middleware.After(ctxx)
-		}()
-	}
-
 	return p.conn.PublishMsg(msg)
 }
 
