e99c8e980888fec1d4b0011721065ff3efdd4246
Eduardo Bonzi da Conceição <eduardo.bonzi@b2wdigital.com>
2023-07-24 09:52:57 -0300

    feat: add opentelemetry plugin for mongo

---
+++ go.mongodb.org/mongo-driver.v1/plugins/contrib/opentelemetry/otelmongo.v1/doc.go
@@ -0,0 +1,5 @@
+// This package implements integration between
+// the official go mongodb driver (https://github.com/mongodb/mongo-go-driver) and OpenTelemetry.
+//
+// import "github.com/americanas-go/ignite/go.mongodb.org/mongo-driver.v1/plugins/contrib/opentelemetry/otelmongo.v1
+package otelmongo
