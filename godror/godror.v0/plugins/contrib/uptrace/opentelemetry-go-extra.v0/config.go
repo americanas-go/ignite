c4a894cd35ce75c1b83e97175b8900f9cef89212
João Paulo Faria <jpfaria@gmail.com>
2023-12-19 18:15:12 -0300

    feat: adds otelsql

---
+++ godror/godror.v0/plugins/contrib/uptrace/opentelemetry-go-extra.v0/config.go
@@ -0,0 +1,19 @@
+package otelsql
+
+import (
+	"github.com/americanas-go/config"
+	"github.com/americanas-go/ignite/godror/godror.v0"
+)
+
+const (
+	root    = godror.PluginsRoot + ".datadog"
+	enabled = ".enabled"
+)
+
+func init() {
+	ConfigAdd(root)
+}
+
+func ConfigAdd(path string) {
+	config.Add(path+enabled, true, "enable/disable datadog integration")
+}
